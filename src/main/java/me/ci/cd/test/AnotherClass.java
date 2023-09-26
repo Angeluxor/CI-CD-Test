d999b7f52cf65095d6f544b43407c4f0a3e580db
Angeluxor <idmiguelangel123@gmail.com>
2023-09-25 20:53:14 -0500

    Create AnotherClass.java

---
+++ src/main/java/me/ci/cd/test/AnotherClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package me.ci.cd.test;
+
+/**
+ *
+ * @author idmig
+ */
+public class AnotherClass {
+    
+    private String with;
+    private int Another;
+    private boolean things;
+    
+    
+}
