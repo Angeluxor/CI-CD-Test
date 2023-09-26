a4fe26ec639fec5ec0aadd41f0d3c201efcc6d5a
Angeluxor <idmiguelangel123@gmail.com>
2023-09-25 19:02:14 -0500

    Initial commit

---
+++ src/main/java/me/ci/cd/test/CICDTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.ci.cd.test;
+
+/**
+ *
+ * @author idmig
+ */
+public class CICDTest {
+
+    public static void main(String[] args) {
+        System.out.println("A commit has been made.");
+    }
+}
