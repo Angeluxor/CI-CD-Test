3d2d1763e052997200069dbb5c42ab08894ead9d
Angeluxor <idmiguelangel123@gmail.com>
2023-10-02 19:30:22 -0500

    Changes

---
+++ src/main/java/main/CICDTest.java
@@ -0,0 +1,10 @@
+package main;
+
+public class CICDTest {
+
+    public static void main(String[] args) {
+        System.out.println("A commit has been made.");
+        
+        System.out.println(new CICDTest().getClass().getName());
+    }
+}
